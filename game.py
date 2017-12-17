a120ece6c281c856d63b044677289fe4f32be50e
PiyushMankad <32595688+PiyushMankad@users.noreply.github.com>
2017-12-17 16:49:30 +0530

    Update game.py

--- game.py
+++ game.py
@@ -32,6 +32,7 @@ foodspawn = True
 direction = 'RIGHT'
 changeto = direction
 score = 0
+
 #function-> game over
 def gameover():
     myFont = pygame.font.SysFont('AR Blanca',56)
@@ -44,7 +45,7 @@ def gameover():
     time.sleep(5)
     pygame.quit()
     sys.exit()
-    
+#function-> show score    
 def showscore(choice=1):
     sFont = pygame.font.SysFont('AR Blanca',24)
     ssurf = sFont.render('Score : {0}'.format(score), True, black)
@@ -73,7 +74,7 @@ while True:
             if event.key == pygame.K_ESCAPE:
                 pygame.event.post(pygame.event.Event(pygame.QUIT))
                 
-                
+  #for not moving in the opposite direction              
     if changeto == 'RIGHT' and not direction == 'LEFT':
         direction ='RIGHT'
     if changeto == 'LEFT' and not direction == 'RIGHT':
@@ -83,7 +84,7 @@ while True:
     if changeto == 'DOWN' and not direction == 'UP':
         direction ='DOWN'
         
-    #snake position   
+    #snake head position   
     if direction == 'RIGHT':
         head[0] += 10
     if direction == 'LEFT':
@@ -93,37 +94,37 @@ while True:
     if direction == 'UP':
         head[1] -= 10
         
-    #body
+    #body addition and deletion
     body.insert(0,list(head))
     if head[0] == food[0] and head[1] == food[1]:
         score +=1
         foodspawn = False
     else:
         body.pop()
-        
+     #food is eaten   
     if foodspawn == False:
         food = [random.randrange(1,72)*10,random.randrange(1,48)*10]
     foodspawn = True
-    
+    # background color
     surface.fill(white)
     for pos in body:
         pygame.draw.rect(surface, blue, pygame.Rect(pos[0],pos[1],10,10))
         
     pygame.draw.rect(surface, orange, pygame.Rect(food[0],food[1],10,10))
         
-    #bound
+    #snake out of bounds
     if head[0] > 710 or head[0] < 0:
         gameover()
     if head[1] > 470 or head[1] < 0:
         gameover()
-        
+     #collision with itself   
     for block in body[1:]:
         if head[0] == block[0] and head[1] == block[1]:
             gameover()
     
-    pygame.display.flip()
+    pygame.display.flip()#updates the screen
     showscore()                     
-    fps.tick(11)
+    fps.tick(11)# 11 times update in a second
 
     
     
